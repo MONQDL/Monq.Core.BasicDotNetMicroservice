09cf4f1162dc77e7203eb1420096951e6d981ed4
NazarovMikhail <nazarovmu@monqlab.com>
2026-01-22 09:22:23 +0500

    Add service collection extension method AddRestHttpPreConfiguredClient.

--- src/Monq.Core.BasicDotNetMicroservice/Extensions/ServiceCollectionExtensions.cs
+++ src/Monq.Core.BasicDotNetMicroservice/Extensions/ServiceCollectionExtensions.cs
@@ -233,6 +233,38 @@ public static class ServiceCollectionExtensions
     }
 
     /// <summary>
+    /// Add preconfigured implementation of <see cref="RestHttpClient"/> with configured address and timeout.
+    /// </summary>
+    /// <typeparam name="TClient"> The type of the typed client interface.</typeparam>
+    /// <typeparam name="TImplementation"> The implementation type of the typed client and <see cref="RestHttpClient"/>.</typeparam>
+    /// <param name="services">The <see cref="IServiceCollection"/>.</param>
+    /// <param name="configuration">The <see cref="IConfiguration"/>.</param>
+    /// <returns>An <see cref="IHttpClientBuilder"/> that can be used to configure the client.</returns>
+    [RequiresUnreferencedCode("Uses AddHttpClient which internally relies on reflection is incompatible with trimming.")]
+    public static IHttpClientBuilder AddRestHttpPreConfiguredClient<TClient, [DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicConstructors)] TImplementation>(
+        this IServiceCollection services,
+        IConfiguration configuration)
+        where TClient : class
+        where TImplementation : RestHttpClient, TClient
+    {
+        var baseUri = new Uri(
+            configuration.GetValue<string>(nameof(AppConfiguration.BaseUri))
+            ?? throw new Exception("Grpc BaseUri not found at configuration."));
+        var httpClientBuilder = services.AddHttpClient<TClient, TImplementation>(
+            client => 
+            { 
+                client.BaseAddress = baseUri;
+
+                // To reuse the HttpClient instance, we will use the cancellation token to manage timeouts.
+                // To do this, you need to set the main timeout to the maximum value,
+                // because it will override the value specified in the cancellation token.
+                client.Timeout = System.Threading.Timeout.InfiniteTimeSpan;
+            });
+
+        return httpClientBuilder;
+    }
+
+    /// <summary>
     /// Add preconfigured gRPC client with configured call credentials.
     /// </summary>
     /// <typeparam name="TClient">The type of the gRPC client. The type specified will be registered in the service collection as
